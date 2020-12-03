5da1467166800d6f563d70c0effd769119b5f4b0
zhenhux.xue <zhenhua.xue@vhall.com>
2020-12-03 19:30:39 +0800

    bug fix

--- vhall_class_sdk/app/src/main/java/com/vhall/classsdk/demo/ClassApplication.java
+++ vhall_class_sdk/app/src/main/java/com/vhall/classsdk/demo/ClassApplication.java
@@ -25,9 +25,8 @@ public class ClassApplication extends Application {
         device = Build.BOARD + Build.DEVICE + Build.SERIAL;//SERIAL  串口序列号 保证唯一值
         Log.e(Constant.TAG, " device = " + device);
         context = this;
-        //线上 a39259f4283861c1eb789d4b018c5f02   edf494f08cceee9f3dc74a44bc994fc7
-        //线下 207038a004fcc7012fec958ce992ef8a   dda37cdf83997044539ad9c35ab87be1
-        VHClass.getInstance().init(context, "a39259f4283861c1eb789d4b018c5f02", "edf494f08cceee9f3dc74a44bc994fc7");
+
+        VHClass.getInstance().init(context, "", "");
     }
 
 }
